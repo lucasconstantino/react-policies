8664e923cffcfe6f6d0cc29ce434c0b20087023b
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2016-07-24 12:30:18 -0300

    Added hiding test for combined policies.

--- __tests__/combine.js
+++ __tests__/combine.js
@@ -9,7 +9,7 @@ import Policy, { combine as policies } from '../src'
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
 describe('Combine', () => {
-  fit('should show component if policy validates true', async () => {
+  it('should show component if combined policy validates true', async () => {
     const Dumb = props => (<div />)
     const first = Policy({ test: props => props.first })
     const second = Policy({ test: props => props.second })
@@ -19,5 +19,17 @@ describe('Combine', () => {
     expect(Wrapper.find(Dumb).length).toBe(0)
     await sleep(1)
     expect(Wrapper.find(Dumb).length).toBe(1)
+  })
+
+  it('should hide component if at least one combined policy validates false', async () => {
+    const Dumb = props => (<div />)
+    const first = Policy({ test: props => props.first })
+    const second = Policy({ test: props => props.second })
+    const PoliciedComponent = policies(first, second)(Dumb)
+    const Wrapper = mount(<PoliciedComponent first />)
+
+    expect(Wrapper.find(Dumb).length).toBe(0)
+    await sleep(1)
+    expect(Wrapper.find(Dumb).length).toBe(0)
   })
 })
